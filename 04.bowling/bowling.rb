596261a8abffe7fe23eb716de0201692a9c556ce
eatplaynap <eatplaynap329@gmail.com>
2021-02-04 16:28:03 +0900

    ボウリングシステム完成版(rubocop確認済み)

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,20 +1,21 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.chars
-frame = []
+shots = []
 frames = []
 
 scores.each do |s|
-  if s == 'X'
-    frame << 10
-  else
-    frame << s.to_i
-  end
+  shots << if s == 'X'
+             10
+           else
+             s.to_i
+           end
 
-  if frame[0] == 10 || frame.count == 2 || frames.count >= 10
-    frames << frame
-    frame = []
+  if shots[0] == 10 || shots.count == 2 || frames.count >= 10
+    frames << shots
+    shots = []
   elsif frames.count == 12
     frames[10].concat(frames[11])
   end
@@ -31,23 +32,23 @@ point = 0
 frames.each_with_index do |frame, i|
   case i
   when 0..7
-    if frame[0] == 10 && frames[i + 1][0] == 10
-      point += frame[0] + frames[i + 1][0] + frames[i + 2][0]
-    elsif frame[0] == 10 && frames[i + 1][0] != 10
-      point += frame[0] + frames[i + 1][0] + frames[i + 1][1]
-    elsif frame[0] != 10 && frame.sum == 10
-      point += frame.sum + frames[i + 1][0]
-    else
-      point += frame.sum
-    end
+    point += if frame[0] == 10 && frames[i + 1][0] == 10
+               frame[0] + frames[i + 1][0] + frames[i + 2][0]
+             elsif frame[0] == 10 && frames[i + 1][0] != 10
+               frame[0] + frames[i + 1][0] + frames[i + 1][1]
+             elsif frame[0] != 10 && frame.sum == 10
+               frame.sum + frames[i + 1][0]
+             else
+               frame.sum
+             end
   when 8
-    if frame[0] == 10
-      point += frame[0] + frames[i + 1][0] + frames[i + 1][1]
-    elsif frame.sum == 10
-      point += frame.sum + frames[i + 1][0]
-    else
-      point += frame.sum
-    end
+    point += if frame[0] == 10
+               frame[0] + frames[i + 1][0] + frames[i + 1][1]
+             elsif frame.sum == 10
+               frame.sum + frames[i + 1][0]
+             else
+               frame.sum
+             end
   when 9
     point += frame.sum
   end
